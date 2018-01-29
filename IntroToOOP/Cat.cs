3bd002d64c14e3732db5f8b23ff2a838222dd15d
AHoobler <ashley.hoobler@gmail.com>
2018-01-29 11:40:38 -0500

    added cat class and attributes

--- IntroToOOP/Cat.cs
+++ IntroToOOP/Cat.cs
@@ -8,5 +8,44 @@ namespace IntroToOOP
 {
     class Cat
     {
+        // I need states
+
+
+        private string name;        // can only be accessed here
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        // I need behaviors
+
+        // constuctor
+        public Cat()
+        {
+            // default constuctor, takes no parameters (nothing in the parenthesis)
+
+        }
+
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+
+
+        //methods
+        public void Eat() 
+        {
+            if (isHungry)
+            {
+                isHungry = false;
+            }
+            Console.WriteLine("Is the cat hungry" + isHungry);
+        }
     }
 }
